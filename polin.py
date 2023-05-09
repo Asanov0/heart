e67114c55cf3b86fe4fb67967b671b3670ef0040
Azatbek <mairanbekuuluazatbek@gmail.com>
2023-05-10 01:13:18 +0600

    first commit

---
+++ polin.py
@@ -0,0 +1,9 @@
+s = "kazak"
+def palindrom(s):
+    if len(s) <=1: 
+        return "Да палиндром"
+    if s[0] != s[-1]:
+        return "Не палиндром"
+    return palindrom(s[1: -1])
+
+print(palindrom(s))
